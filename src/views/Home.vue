5aac16420a507cc5103afb560e0e73cf02b41a9a
kenpos13@gmail.com <kenpos13@gmail.com>
2020-12-31 15:54:39 +0900

    FirstCommit

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+    <h3>Home</h3>
+    <button @click="toUsers">Usersのページへ移動</button>
+</div>
+</template>
+
+<script>
+    export default{
+        methods:{
+            toUsers(){
+                this.$router.push( { 
+                    name : 'users-id-props', 
+                    params: { id: 1 }
+                } );
+            }
+        }
+    };
+</script>
---
+++ src/views/Users.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+    <h3>Users</h3>
+    <router-link to="/users/1" >ユーザー1</router-link>
+    <router-link to="/users/2" >ユーザー2</router-link>
+    <hr>
+    <h1>User No. {{ id }}</h1>
+    <router-link :to="'/users/' + (Number(id) + 1) + '/profile'">次のユーザ</router-link>
+    <router-link :to="{ name : 'users-id-props', params: { id:(Number(id) + 1) }, query: {lang :'ja',page:'2'} }">次のユーザ</router-link>
+    <router-view></router-view>
+</div>
+</template>
+
+<script>
+export default {
+    props: ["id"],
+};
+</script>
