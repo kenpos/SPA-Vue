1dd5ab27d6bd389429be918249e51e029bd5f053
kenpos13@gmail.com <kenpos13@gmail.com>
2021-01-02 03:08:11 +0900

    Add Vuex

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import store from "./store";
+
 
 Vue.config.productionTip = false;
 
@@ -14,5 +16,6 @@ router.beforeEach((to, from, next )=> {
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount("#app");
